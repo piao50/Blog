5e39384a3257845cf6e915a551b9cdc22a19c550
Gu Shi <piao50@gmail.com>
2020-12-25 16:38:43 +0800

    Blog

---
+++ learn-java/hello.java
@@ -0,0 +1,8 @@
+public class Hello
+{
+    public static void main(String args[])
+    {
+	int cnt = 10; while(cnt-- > 0)
+	System.out.println("Hello, gushi!" + cnt);
+    }
+}
---
+++ learn-js/hello.js
@@ -0,0 +1,31 @@
+console.log('hello, gushi');
+
+function f(a, b) { return a * b; }
+
+var x, y;
+x = 1;
+y = 5;
+z = ( x + y ) * 2;
+
+function createImage(name){
+    return new Image(name);
+}
+
+/*
+var http = require('http'); 
+http.createServer(function (req, res) { 
+  res.writeHead(200, {'Content-Type': 'text/plain'}); 
+  res.end('Hello World\n'); 
+}).listen(9999, "0.0.0.0"); 
+console.log('Server running at http://127.0.0.1:8001/'); 
+*/
+
+const http = require('http');
+
+
+(async () => {
+    var cnt = 5; while(cnt-- > 0)
+    console.log('gushi is here!' + cnt);
+})()
+
+console.log('bye, gushi');
