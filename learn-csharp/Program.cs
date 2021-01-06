1a62ec341fac0409285cbab29aaac77c19429891
Gu Shi <piao50@gmail.com>
2021-01-06 09:03:47 +0800

    weigu

---
+++ learn-csharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace learn_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+	    var cnt = 10;
+	    while(cnt-->0) Console.WriteLine(cnt);
+
+	    Console.WriteLine("Bye Gu Shi.");
+        }
+    }
+}
