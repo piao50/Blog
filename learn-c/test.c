8ec3e6a14bcff80188fe8d871614f93e5255e58a
Gu Shi <piao50@gmail.com>
2021-01-06 13:02:45 +0800

    weigu

--- learn-c/test.c
+++ learn-c/test.c
@@ -3,7 +3,16 @@
 int main()
 {
   printf("hello, gushi!\r\n");
-  for(int i = 0; i < 10; i++) printf("%d\r\n", i);
+  // for(int i = 0; i < 10; i++) printf("%d\r\n", i);
+
+  int nums[10] = {1, 10, 6, 296, 111, 23, 0, 34, 999};
+
+  int num;
+  printf("Input an integer: ");
+  scanf("%d", &num);
+  printf("Just your enter: %d\r\n", num);
+  
+  
   printf("bye, gushi!\r\n");
   return 0;
 }
---
+++ learn-js/node.js
@@ -0,0 +1,33 @@
+console.log(Date());
+
+function hello()
+{
+    var cnt = 10; while(cnt-->0)
+    console.log('hello');
+}
+
+function hello1(p1)
+{
+    console.log('hello ' + p1);
+}
+
+function times(a,b) { return a * b; }
+
+hello();
+hello1(1);
+hello1(times(5,8));
+
+
+// string is here
+var x = "Bill Gates";
+console.log(x);
+console.log(x.length);
+
+var y = "Hello, Gu Shi";
+var pos = y.indexOf("Gu");
+console.log(pos);
+var pos1 = y.search("Gu");
+console.log(pos1);
+console.log(y.substr(pos, 6));
+y = y.concat(" from China");
+console.log(y);
