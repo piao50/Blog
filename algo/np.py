f8cc37bcd51845c60148d4c64c5ca6cc82699271
Gu Shi <piao50@gmail.com>
2021-01-14 10:09:05 +0800

    weigu

--- algo/np.py
+++ algo/np.py
@@ -46,7 +46,7 @@ mu, sigma = 2, 0.5
 v = rg.normal(mu, sigma, 10000)
 plt.hist(v, bins=50, density=1)
 (n, bins) = np.histogram(v, bins=50, density=True)
-plt.clear()
+#plt.clear()
 plt.plot(.5*(bins[1:]+bins[:-1]),n)
 plt.savefig('hist.jpg')
 
