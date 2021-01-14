88e28ffa83b0994a4d04d8d3cb619a30a39cc07a
Gu Shi <piao50@gmail.com>
2021-01-14 09:42:00 +0800

    weigu

--- data/myspider.py
+++ data/myspider.py
@@ -4,6 +4,7 @@ class BlogSpider(scrapy.Spider):
     name = 'Blog'
     start_urls = ['https://blog.scrapinghub.com']
     start_urls = ['https://www.baidu.com']
+    start_urls = ['https://news.baidu.com']
     
     def parse(self, response):
         for title in response.css('.post-header>h2'):
@@ -13,17 +14,33 @@ class BlogSpider(scrapy.Spider):
             yield response.follow(next_page, self.parse)
 
 
-# //*[@id="s-top-left"]/a[1]            
         for m in response.xpath('*[@id="s-top-left"]/a[1]'):
             print('href:', m)
 
         content = response.xpath('/html/head/title/text()')
         content = response.xpath('//div')
         content = response.xpath('/html/body/div[1]/div[1]/div[1]/div[2]/a/@href')
+        content = response.xpath('/html/body/div[2]/div[2]/div[2]/div/ul/li[2]')
+        content = response.xpath('//*[@id="channel-all"]/div/ul/li/a/@href')
         print('len:', len(content))
+        cnt = 0
         for c in content:
             print(c.extract())
-            yield {'title': c.extract()}
-#        print(content.extract())
+            #yield {'url': c.extract()}
+            url = self.start_urls[0] + c.extract()
+            print(url)
+            yield {'url:': url}
+            cnt = cnt + 1
+            if (cnt > 100) :
+                break
+            yield response.follow(self.start_urls[0] + c.extract(), self.parse_s)
             
+#                 
         print('parse invoked')
+
+    def parse_s(self, response):
+        print('parse_s')
+        print(response.xpath('/html/head/title').extract())
+        pass
+
+    
